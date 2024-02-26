99dbfc8bf2bca945217035b1a931ce95b1815d0b
ranuga13 <ranugajaya13@gmail.com>
2024-02-26 22:28:39 +0530

    Update sidebar.jsx

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useSyncExternalStore} from 'react'
+import React, { useState, useSyncExternalStore } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import useDarkMode from "../hooks/useDarkMode";
 import boardsSlice from "../redux/boardsSlice";
@@ -8,13 +8,9 @@ import darkIcon from "../assets/icon-dark-theme.svg";
 import lightIcon from "../assets/icon-light-theme.svg";
 import showSidebarIcon from "../assets/icon-show-sidebar.svg";
 import hideSidebarIcon from "../assets/icon-hide-sidebar.svg";
-import AddEditBoardModal from '../modals/AddEditBoardModal';
+import AddEditBoardModal from "../modals/AddEditBoardModal";
 
-
-
-
-
-function SideBar({setIsSideBarOpen , isSideBarOpen}) {
+function SideBar({ setIsSideBarOpen, isSideBarOpen }) {
   const dispatch = useDispatch();
   const [colorTheme, setTheme] = useDarkMode();
   const [darkSide, setDarkSide] = useState(
@@ -28,64 +24,55 @@ function SideBar({setIsSideBarOpen , isSideBarOpen}) {
 
   const boards = useSelector((state) => state.boards);
 
-  
-  const [isBoardModalOpen,setBoardModalOpen] =useState(false)
+  const [isBoardModalOpen, setBoardModalOpen] = useState(false);
 
   return (
     <div>
       <div
-      className={isSideBarOpen ? 'min-w-[261px] bg-white dark:bg-[#2b2c37] fixed top-[72px] h-screen items-center left-0 z-20' : 'bg-[#635fc7] dark:bg-[#2bbcc37] dark:hover:bg-[#635fc7] top-auto bottom -10 justify-center items-center hover:opacity-80 cursor-pointer p-0 transition-duration:300 transform fixed w-[56px] h-[48px] rounder-r-full'}
+        className={
+          isSideBarOpen
+            ? "min-w-[261px] bg-white dark:bg-[#2b2c37] fixed top-[72px] h-screen items-center left-0 z-20"
+            : "bg-[#635fc7] dark:bg-[#2bbcc37] dark:hover:bg-[#635fc7] top-auto bottom -10 justify-center items-center hover:opacity-80 cursor-pointer p-0 transition-duration:300 transform fixed w-[56px] h-[48px] rounder-r-full"
+        }
       >
         <div>
           {/*Rewrite Model*/}
 
-          {
-            isSideBarOpen && (
-              <div className=' bg-white dark:bg-[#2b2c37 w-full py-4 rounded-xl ]'>
-                <h3 
-                className =' dark:text-gray-300 text-gray-600 font-semibold mx-4 mb-8 '
-                >
-                  ALL BOARDS ({boards?.length})
-                </h3>
+          {isSideBarOpen && (
+            <div className=" bg-white dark:bg-[#2b2c37 w-full py-4 rounded-xl ]">
+              <h3 className=" dark:text-gray-300 text-gray-600 font-semibold mx-4 mb-8 ">
+                ALL BOARDS ({boards?.length})
+              </h3>
 
-                <div
-                className=' flex flex-col h-[70vh] justify between'
-                >
-                  <div>
-                    {boards.map((board, index) =>(
-                      <div
-                      className ={` flex items-baseline space-x-2 px-5 mr-8 rounded-r-full duration-500 ease-in-out py-4 cursor-pointer hover:bg-[#635fc71a] hover:text-[#635fc7] dark:hover:bg-white dark:hover:text-[#635fc7] dark:text-white ${board.isActive && " bg-[#635fc7] rounded-r-full text-white mr-8" }   `}
+              <div className=" flex flex-col h-[70vh] justify between">
+                <div>
+                  {boards.map((board, index) => (
+                    <div
+                      className={` flex items-baseline space-x-2 px-5 mr-8 rounded-r-full duration-500 ease-in-out py-4 cursor-pointer hover:bg-[#635fc71a] hover:text-[#635fc7] dark:hover:bg-white dark:hover:text-[#635fc7] dark:text-white ${
+                        board.isActive &&
+                        " bg-[#635fc7] rounded-r-full text-white mr-8"
+                      }   `}
                       key={index}
                       onClick={() => {
-                        dispatch(boardsSlice.actions.setBoardActive({index}))
+                        dispatch(boardsSlice.actions.setBoardActive({ index }));
                       }}
-                      >
-                        <img src= {boardIcon} className='h-4'/>*
-                        <p className='text-ig font-blood'>
-                          {board.name}
-                        </p>
-
-                      </div>
-
-                    ))}
-
-                    <div
-                    onClick={() => {
-                      setBoardmodalOpen(true);
-                    }}
-                     className=' flex items-baseline space-x-2 mr-8 rounded-r-full duration-500 ease-in-out cursor-pointer text-[#635fc7] px-5 py-4 hover:bg-[#635fc71a] hover:text[#635fc7] dark:hover:bg-white'
                     >
-                      <img src= {boardIcon} className='h-4'/>*
-                        <p className='text-ig font-blood'>
-                          Create New Board
-                        </p>
-
+                      <img src={boardIcon} className="h-4" />
+                      <p className="text-ig font-blood">{board.name}</p>
                     </div>
+                  ))}
 
-                  </div>
                   <div
-                  className='mx-2 p-4 relative space-x-2 bg-slate-100 dark:bg-[#20212c] flex justify-center items-center rounded-lg'
-                  >  
+                    onClick={() => {
+                      setBoardModalOpen(true);
+                    }}
+                    className=" flex items-baseline space-x-2 mr-8 rounded-r-full duration-500 ease-in-out cursor-pointer text-[#635fc7] px-5 py-4 hover:bg-[#635fc71a] hover:text[#635fc7] dark:hover:bg-white"
+                  >
+                    <img src={boardIcon} className="h-4" />
+                    <p className="text-ig font-blood">Create New Board</p>
+                  </div>
+                </div>
+                <div className="mx-2 p-4 relative space-x-2 bg-slate-100 dark:bg-[#20212c] flex justify-center items-center rounded-lg">
                   <img src={lightIcon} alt="sun indicating light mode" />
 
                   <Switch
@@ -102,58 +89,41 @@ function SideBar({setIsSideBarOpen , isSideBarOpen}) {
                       } inline-block h-4 w-4 transform rounded-full bg-white transition`}
                     />
                   </Switch>
-      
+
                   <img src={darkIcon} alt="moon indicating dark mode" />
-
-                  </div>
-
-
                 </div>
-
-
-
               </div>
+            </div>
+          )}
+          {/*Sidebar hide/show toggle*/}
 
-
-
-            )
-          }
-         { /*Sidebar hide/show toggle*/}
-
-         {isSideBarOpen ?(
-          <div 
-          onClick={() => setIsSideBarOpen(state => !state)}
-          className=' flex items-center mt-2 absolute bottom-16 text -lg font-bold rounded -r-full hover: text-[#635fc7] cursor-pointer mr-6 mb-8 px-8 py-4 hover:bg-[#635fc71a] dark:hover:bg-white space-x-2 justify-center my-4 text-gray-500'
-          > 
-          <img src={hideSidebarIcon} className=' min-w-[20px]' alt="hideSidebarIcon" />
-          {isSideBarOpen && <p> Hide sidebar </p>}
-            
-          </div>
-         )
-          :
-          <div 
-          onClick={() => setIsSideBarOpen(state => !state)}
-          className='absolute p-5'>
-            <img src={showSidebarIcon} alt="showSidebarIcon" />
-
-          </div>
-        }
-          
+          {isSideBarOpen ? (
+            <div
+              onClick={() => setIsSideBarOpen((state) => !state)}
+              className=" flex items-center mt-2 absolute bottom-16 text -lg font-bold rounded -r-full hover: text-[#635fc7] cursor-pointer mr-6 mb-8 px-8 py-4 hover:bg-[#635fc71a] dark:hover:bg-white space-x-2 justify-center my-4 text-gray-500"
+            >
+              <img
+                src={hideSidebarIcon}
+                className=" min-w-[20px]"
+                alt="hideSidebarIcon"
+              />
+              {isSideBarOpen && <p> Hide sidebar </p>}
+            </div>
+          ) : (
+            <div
+              onClick={() => setIsSideBarOpen((state) => !state)}
+              className="absolute p-5"
+            >
+              <img src={showSidebarIcon} alt="showSidebarIcon" />
+            </div>
+          )}
         </div>
-
       </div>
-      {
-        isBoardModalOpen &&
-        <AddEditBoardModal
-        type='add'
-        setBoardModalOpen={setBoardModalOpen}
-
-        />
-      }
-
-
+      {isBoardModalOpen && (
+        <AddEditBoardModal type="add" setBoardModalOpen={setBoardModalOpen} />
+      )}
     </div>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
