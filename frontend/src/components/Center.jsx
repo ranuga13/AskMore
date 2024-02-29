df46e1168e038017faa320d718b42d1a39126083
ranuga13 <ranugajaya13@gmail.com>
2024-02-29 22:07:51 +0530

    Fixed Errors in Center Component

--- frontend/src/components/Center.jsx
+++ frontend/src/components/Center.jsx
@@ -5,18 +5,17 @@ import Column from "./Column";
 import EmptyBoard from "./EmptyBoard";
 import SideBar from "./SideBar";
 
-function Center(boardModalOpen,setBoardModalOpen) {
+function Center(boardModalOpen, setBoardModalOpen) {
   const boards = useSelector((state) => state.boards);
   const board = boards.find((board) => board.isActive === true);
   const columns = board.columns;
-  {const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);}
+  const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
   const [isSideBarOpen, setIsSideBarOpen] = useState(true);
 
   const [windowSize, setWindowSize] = useState([
     window.innerWidth,
     window.innerHeight,
   ]);
-  
 
   useEffect(() => {
     const handleWindowResize = () => {
@@ -30,53 +29,49 @@ function Center(boardModalOpen,setBoardModalOpen) {
     };
   });
 
-  
-
   return (
-    <div className={
-      windowSize[0] >= 768 && isSideBarOpen
-        ? " bg-[#f4f7fd]  scrollbar-hide h-screen flex dark:bg-[#20212c]  overflow-x-scroll gap-6  ml-[261px]"
-        : "bg-[#f4f7fd]  scrollbar-hide h-screen flex    dark:bg-[#20212c] overflow-x-scroll gap-6 "
-    }
+    <div
+      className={
+        windowSize[0] >= 768 && isSideBarOpen
+          ? " bg-[#f4f7fd]  scrollbar-hide h-screen flex dark:bg-[#20212c]  overflow-x-scroll gap-6  ml-[261px]"
+          : "bg-[#f4f7fd]  scrollbar-hide h-screen flex    dark:bg-[#20212c] overflow-x-scroll gap-6 "
+      }
     >
-    {windowSize[0] >= 768 && (
-      <SideBar isSideBarOpen={isSideBarOpen} setIsSideBarOpen ={setIsSideBarOpen} />
-    )}
+      {windowSize[0] >= 768 && (
+        <SideBar
+          isSideBarOpen={isSideBarOpen}
+          setIsSideBarOpen={setIsSideBarOpen}
+        />
+      )}
 
-    {/* columns section */}
-    {columns.length > 0 ? (
-      <>
-        {columns.map((col, index) => (
+      {/* columns section */}
+      {columns.length > 0 ? (
+        <>
+          {columns.map((col, index) => (
             <Column key={index} colIndex={index} />
           ))}
           <div
-          onClick={() => {
-            setIsBoardModalOpen(true)
-          }}
-          className=' h-screen dark:bg-[#2b2c3740] flex justify-center items-center font-bold text-2xl hover:text-[#635fc7] transition duration-300 cursor-pointer bg-[#e9effa] scrollbar-hide mb-2 mx-5 pt-[90px] min-w-[280px] text-[#828fa3] mt-[135px] rounded-lg'
+            onClick={() => {
+              setIsBoardModalOpen(true);
+            }}
+            className=" h-screen dark:bg-[#2b2c3740] flex justify-center items-center font-bold text-2xl hover:text-[#635fc7] transition duration-300 cursor-pointer bg-[#e9effa] scrollbar-hide mb-2 mx-5 pt-[90px] min-w-[280px] text-[#828fa3] mt-[135px] rounded-lg"
           >
             +New column
-
           </div>
-      </>
-  
-    ):(<>
-      <EmptyBoard type='edit' />
-    </>
-    )
-  }
-  {
-    boardModalOpen && (
-      <AddEditBoardModal
-      type='edit'
-      setBoardModalOpen={setBoardModalOpen}
-      />
-    )
-  }
-
-    
+        </>
+      ) : (
+        <>
+          <EmptyBoard type="edit" />
+        </>
+      )}
+      {isBoardModalOpen && (
+        <AddEditBoardModal
+          type="edit"
+          setBoardModalOpen={setIsBoardModalOpen}
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default Center
+export default Center;
