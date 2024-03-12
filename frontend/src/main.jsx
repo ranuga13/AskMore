8212efcf00a4d3b73b6156b8605f686ee4246ba8
ranuga13 <ranugajaya13@gmail.com>
2024-03-12 20:48:10 +0530

    Revert "User Profile Added but db not connected"
    This reverts commit 836d864a6db98a45c8f28504a5468b4f323e2ab3.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,20 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { ClerkProvider } from '@clerk/clerk-react'
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
- 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App mode="modal" redirectUrl="/app" />
-    </ClerkProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 )
