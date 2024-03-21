fe44e0a4830380f1cf603b4eb841b8f0cb78d1e8
Hans <khansikaseneviratne@gmail.com>
2024-03-21 09:10:48 +0530

    Added mark questions as read function

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -9,6 +9,8 @@ function Task({ taskIndex, colIndex }) {
   const col = columns.find((col, i) => i === colIndex);
   const task = col.tasks.find((task, i) => i === taskIndex);
   const [isTaskModalOpen, setIsTaskModalOpen] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(task.isCompleted);
+
 
   let completed = 0;
   let subtasks = task.subtasks;
@@ -25,20 +27,45 @@ function Task({ taskIndex, colIndex }) {
     )
   }
 
+  const handleToggleCompleted = () => {
+    setIsCompleted(!isCompleted);
+  };
+
   return (
     <div>
       <div
         onDragStart={handleOnDrag}
         draggable
-        onClick={() => {
-          setIsTaskModalOpen(true);
-        }}
+        
         className=" w-[280px] first:my-5 rounded-lg  bg-white  dark:bg-[#2b2c37] shadow-[#364e7e1a] py-6 px-3 shadow-lg hover:text-[#50ccc8] dark:text-white dark:hover:text-[#50ccc8] cursor-pointer "
       >
-        <p className=" font-bold tracking-wide ">{task.title}</p>
+        <p onClick={() => {
+            setIsTaskModalOpen(true);
+          }}
+          className={`font-bold tracking-wide ${
+            isCompleted ? "line-through" : ""
+          }`}
+          title="Click to see question details" // Added title attribute
+
+          >
+            {task.title}</p>
+
         <p className=" font-bold text-xs tracking-tighter mt-2 text-gray-500">
           {/* {completed} of {subtasks.length} completed tasks */}
         </p>
+
+        <label className="flex items-center mt-2 cursor-pointer">
+          <input
+            type="checkbox"
+            checked={isCompleted}
+            onChange={handleToggleCompleted}
+            className="mr-2 cursor-pointer"
+          />
+          <p className="font-bold text-xs tracking-tighter text-gray-500">
+            {isCompleted ? "Responded" : "Mark as Responded"}
+          </p>
+        </label>
+
       </div>
       {
         isTaskModalOpen && (
