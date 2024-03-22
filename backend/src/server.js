b05beecc50abdcf264d95188bf2d6cbeadf9018f
ranuga13 <ranugajaya13@gmail.com>
2024-03-22 20:53:07 +0530

    Made the database realtime.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const cors = require("cors");
 const { connectToDatabase } = require("./config/db.js");
 const boardsRoutes = require("./routes/users.js");
+// const User = require("./models/User.js");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(
   express.urlencoded({
@@ -19,21 +25,55 @@ app.use(
 // routes
 app.use("/api/users", boardsRoutes);
 
-// Define a simple route
-// app.get("/", (req, res) => {
-//   res.send("Hello, this is the root path!");
-// });
+io.on("connection", (socket) => {
+  console.log(`A user connected with socket id ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`A user disconnected with socket id ${socket.id}`);
+  });
+});
 
 connectToDatabase()
   .then(() => {
-    app.listen(3000, () => {
+    const User = require("./models/userModel.js");
+    const changeStream = User.watch();
+
+    // changeStream.on("change", (change) => {
+    //   console.log("Change detected:", change);
+    //   io.emit("change", change);
+    // });
+
+    changeStream.on("change", async (change) => {
+      console.log("Change detected:", change);
+
+      if (change.operationType === "update") {
+        console.log("Operation type is update");
+
+        // Convert _id to string for comparison
+        const userId = change.documentKey._id.toString();
+        if (userId === "321") {
+          console.log("Updated user with userid 321");
+          try {
+            // Fetch the updated user with userid 321 from the database
+            const updatedUser = await User.findById(userId);
+            console.log("Updated user:", updatedUser);
+
+            if (updatedUser) {
+              // Emit the boards data for the user with userid 321
+              io.emit("change", updatedUser.boards);
+              console.log("Emitted updated boards data:", updatedUser.boards);
+            }
+          } catch (error) {
+            console.error("Error fetching updated user:", error);
+          }
+        }
+      }
+    });
+
+    server.listen(3000, () => {
       console.log("Node API app is running on port 3000");
     });
   })
   .catch((error) => {
     console.error("Error starting the app:", error.message);
   });
-
-// app.get("/", (req, res) => {
-//   res.status(201).json({ message: "connected to backend" });
-// });
