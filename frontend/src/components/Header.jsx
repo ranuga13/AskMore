c35bec84d2b72d7c2e0bb77689bc1cc157ddbfcd
ranuga13 <ranugajaya13@gmail.com>
2024-03-22 23:46:58 +0530

    Made changes in the UI.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,7 +13,10 @@ import boardsSlice from "../redux/boardsSlice";
 import ShareModal from "../modals/ShareModal";
 import { deleteBoard } from "../redux/boardsSlice";
 import { selectActiveBoardId } from "../utils/selectors";
-
+import darkIcon from "../assets/icon-dark-theme.svg";
+import lightIcon from "../assets/icon-light-theme.svg";
+import { Switch } from "@headlessui/react";
+import useDarkMode from "../hooks/useDarkMode";
 import { useClerk } from "@clerk/clerk-react";
 import {
   SignedIn,
@@ -65,6 +68,15 @@ function Header({ setBoardModalOpen, boardModalOpen }) {
     setIsElipsisOpen(false);
     setBoardType("add");
   };
+  const [colorTheme, setTheme] = useDarkMode();
+  const [darkSide, setDarkSide] = useState(
+    colorTheme === "light" ? true : false
+  );
+
+  const toggleDarkMode = (checked) => {
+    setTheme(colorTheme);
+    setDarkSide(checked);
+  };
 
   const boardName = board?.name || "Default Name";
   return (
@@ -109,6 +121,26 @@ function Header({ setBoardModalOpen, boardModalOpen }) {
           >
             +
           </button>
+          <div className=" mx-2 p-2  space-x-2 bg-slate-100 dark:bg-[#20212c] flex justify-center items-center rounded-lg">
+            <img src={lightIcon} alt="sun indicating light mode" />
+
+            <Switch
+              checked={darkSide}
+              onChange={toggleDarkMode}
+              className={`${
+                darkSide ? "bg-[#635fc7]" : "bg-gray-200"
+              } relative inline-flex h-6 w-11 items-center rounded-full`}
+            >
+              <span className="sr-only">Enable notifications</span>
+              <span
+                className={`${
+                  darkSide ? "translate-x-6" : "translate-x-1"
+                } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+              />
+            </Switch>
+
+            <img src={darkIcon} alt="moon indicating dark mode" />
+          </div>
           <div>
             <SignedIn>
               {/* Mount the UserButton component */}
